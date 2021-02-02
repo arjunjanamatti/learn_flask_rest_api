c92d7947866b86db33a61d9d778ba4d7bba124c8
arjunjanamatti <arjunjanamatti@gmail.com>
2021-02-02 13:32:37 +0530

    delete method

--- test_api.py
+++ test_api.py
@@ -10,5 +10,17 @@ import requests
 # response_1 = requests.get('http://127.0.0.1:5000/info/{}'.format(name))
 # print(response_1.json())
 
-response_video = requests.put('http://127.0.0.1:5000/info/10', {'likes':100})
-print(response_video.json())
+# response_video = requests.put('http://127.0.0.1:5000/info/10', {'likes':100})
+# print(response_video.json())
+# video_id = int(input('Enter the video_id: '))
+# response_video_1 = requests.put('http://127.0.0.1:5000/info/{}'.format(video_id), {'likes':100})
+
+data_list = [{'name':'aj', 'views':2, 'likes':10}, 
+             {'name':'bj', 'views':12, 'likes':110},
+             {'name':'cj', 'views':22, 'likes':210},]
+
+for index, data in enumerate(data_list):
+    response = requests.put('http://127.0.0.1:5000/info/{}'.format(index), data)
+
+response = requests.get('http://127.0.0.1:5000/info/11111')
+print('DELETE: ', response.json())
